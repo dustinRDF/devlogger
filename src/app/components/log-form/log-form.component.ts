de3ec9801223dc3b33cf3bb7c6912b8eac94b4ea
dustinRDF <dustin.rdf@gmail.com>
2019-05-30 13:13:30 -0500

    created log observable for async fetch to logform

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogService } from 'src/app/services/log.service';
 
 @Component({
   selector: 'app-log-form',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-form.component.css']
 })
 export class LogFormComponent implements OnInit {
+  id: string;
+  text: string;
+  date: any;
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
+    //Subscribe to the selectedLog observable
+    this.logService.selectedLog.subscribe(log => {
+      if (log.id !== null) {
+        this.id = log.id;
+        this.text = log.text;
+        this.date = log.date;
+      }
+    });
   }
 
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Log } from '../models/Log';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +8,22 @@ import { Log } from '../models/Log';
 export class LogService {
   logs: Log[];
 
+  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+  selectedLog = this.logSource.asObservable();
+
   constructor() { 
     this.logs = [
       {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
-      {id: '1', text: 'Added Bootstrap', date: new Date('12/27/2017 9:33:33')},
-      {id: '1', text: 'Added logs component', date: new Date('12/27/2017 12:00:23')},
+      {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:33:33')},
+      {id: '3', text: 'Added logs component', date: new Date('12/27/2017 12:00:23')},
     ]
   }
 
-  getLogs() {
-    return this.logs;
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
   }
 }
